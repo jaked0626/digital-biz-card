b5380973b3993dd53eaf92598bcf50e3ef68a18c
jaked0626 <jakez0626@gmail.com>
2023-11-07 01:03:47 +0900

    FIX: import image path error

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,12 @@
 import DownloadButton from "./DownloadButton";
 
 export default function Profile() {
+    const base = import.meta.env.BASE_URL;
+    const imagePath = `${base}profile.JPG`;
+
     return (
         <div className="profile">
-            <img src="/profile.JPG" alt="profile photo" className="profile__photo" />
+            <img src={imagePath} alt="profile photo" className="profile__photo" />
             <h1 className="profile__name">Jake Underland</h1>
             <h2 className="profile__title">&#123;PdM/Engineer&#125;</h2>
             <DownloadButton />
