c839d41ff0137ee9e1da9f1747ddc7c4c634923e
jaked0626 <jakez0626@gmail.com>
2023-11-07 00:49:12 +0900

    fixed profile path

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,7 +3,7 @@ import DownloadButton from "./DownloadButton";
 export default function Profile() {
     return (
         <div className="profile">
-            <img src="../../profile.JPG" alt="profile photo" className="profile__photo" />
+            <img src="/profile.JPG" alt="profile photo" className="profile__photo" />
             <h1 className="profile__name">Jake Underland</h1>
             <h2 className="profile__title">&#123;PdM/Engineer&#125;</h2>
             <DownloadButton />
