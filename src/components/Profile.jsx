156a1781bcd241563fea2e53c5907cac47a3b097
jaked0626 <jakez0626@gmail.com>
2023-11-07 13:39:49 +0900

    added banner for social media

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,5 @@
 import DownloadButton from "./DownloadButton";
+import Socials from './Socials';
 
 export default function Profile() {
     const base = import.meta.env.BASE_URL;
@@ -9,6 +10,7 @@ export default function Profile() {
             <img src={imagePath} alt="profile photo" className="profile__photo" />
             <h1 className="profile__name">Jake Underland</h1>
             <h2 className="profile__title">&#123;PdM/Engineer&#125;</h2>
+            <Socials />
             <DownloadButton />
         </div>
     );
---
+++ src/components/Socials.jsx
@@ -0,0 +1,18 @@
+export default function Socials() {
+    return (
+        <div className="profile__socials">
+            <a href="https://github.com/jaked0626">
+                <i className="fab fa-github"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/jake-underland-720126201/">
+                <i className="fab fa-linkedin"></i>
+            </a>
+            <a href="https://www.facebook.com/profile.php?id=100008952614589">
+                <i className="fab fa-facebook"></i>
+            </a>
+            <a href="https://www.instagram.com/jakeydmonkey/">
+                <i className="fab fa-instagram"></i>
+            </a>
+        </div>
+    );
+}
