cda3ed14ed3dc0ba2108f2ca549a86c62f4ae2b0
jaked0626 <jakez0626@gmail.com>
2024-01-01 00:26:53 +0900

    done: added overflow, wip: changing color scheme (leveling up)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,13 +3,27 @@ export default function Home () {
     <div className="home">
     {/* <div className="home__section"> */}
       <h1 className="home__title">
-        About <span className="home__title--green">Me</span>
+        Hello <span className="home__title--green">World,</span>
       </h1>
       <p className="home__text">
-        Hi! I&#39;m Jake, a backend engineer and product manager in the making! 
-        I started my journey in tech studying statistics and data science in college, 
-        and my current primary interests are graph algorithms, distributed systems, 
-        and product creation/management.
+        I&#39;m Jake, a backend engineer and product manager in the making.
+        <br/>
+        {/* My primary interest in life has always been the study of humans, what drives them, and how 
+        these drives shape society. Product managment is the practice of identifying such drives
+        and aiding in their realization through technical innovation. 
+        My focus in life has always been social sciences; the study of what drives human 
+        activity and how it shapes society. Product management is the practice of identifying said 
+        motives and Coming from a social sciences background, my primary interest is in
+        understanding what drives people, the motives which shape our society. 
+        Product creation and management, to me, is the practice of identifying said motives 
+        and   */}
+        My primary interest has been the study of humans, what drives them. 
+        The human drive that tends towards fruition, almost as an independent and organic process. 
+        In their totality they are The absolute spirit journeying through history, 
+        in locality they manifest as fragments of the spirit&#39;s unwavering conviction 
+        pulling the strings behind every creative process. 
+        <br/>
+        Product management is the practice of identifying
       </p>
     {/* </div> */}
     {/* <div className="home__section"> */}
