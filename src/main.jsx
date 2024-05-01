bc43126ca08924a69e0cf4370170222f96d4e425
jaked0626 <jakez0626@gmail.com>
2024-05-01 14:22:11 +0900

    added analytics to deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { PostHogProvider } from 'posthog-js/react';
 import './index.css';
+
+const posthogOptions = {
+  api_host: import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_HOST,
+};
+
+console.log(posthogOptions);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_KEY}
+      options={posthogOptions}
+    >
+      <App />
+    </PostHogProvider>
   </React.StrictMode>,
 );
